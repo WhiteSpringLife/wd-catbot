a4b2dac0a3973e2bdf1da6d0f2f8b9f72ebc04ad
whitesping <whitespringlife@gmail.com>
2021-09-20 03:28:18 +0900

    add covid command

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { Client, Intents } = require('discord.js');
+const { Client, Intents, Collection } = require('discord.js');
 const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
 const config = require('./config.json');
 
@@ -6,10 +6,28 @@ client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
 });
 
+
+
+let commands = new Collection();
+commands.set('ㅊㅍ', require('wd-covid'));
+commands.set('cv', require('wd-covid'));
+
 client.on('messageCreate', message => {
-    if (message.content === 'ping') {
+    if (message.author.bot || message.channel.type == "dm") return;
+    if (!message.content.startsWith(config.prefix)) return;
+
+    let args = message.content.slice(config.prefix.length).trim().split(/ +/g);
+    let cmd = args.shift().toLowerCase();
+
+    if (message.content === config.prefix+'ping') {
         message.channel.send('pong');
     }
+    else if(commands.get(cmd))
+    {
+        commands.get(cmd).then(res => message.channel.send(res));
+    }
 });
+
+
 
 client.login(config.token);
